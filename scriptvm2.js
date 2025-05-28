6f04449e53ef43d32a36b390def7b046c3f89307
Jazully <80542757+Jazullyart@users.noreply.github.com>
2025-05-27 23:55:13 -0600

    Add files via upload

--- scriptvm2.js
+++ scriptvm2.js
@@ -1,53 +1,27 @@
 //Proyectos
 
-var animeost = document.getElementById("animeost");
-var juanga = document.getElementById("juanga");
-var alytum = document.getElementById("alytum");
-var viajemic = document.getElementById("viajemic");
-var esc = document.getElementById("laesc");
+var stdm = document.getElementById("salatorredm");
+var ste = document.getElementById("salatorree");
+var biblio = document.getElementById("biblio30");
 
-animeost.style.display = "block";
-juanga.style.display = "none";
-alytum.style.display = "none";
-viajemic.style.display = "none";
-esc.style.display = "none";
+stdm.style.display = "block";
+ste.style.display = "none";
+biblio.style.display = "none";
 
-function mostrarAnimeOst() {
-  animeost.style.display = "block";
-  juanga.style.display = "none";
-  alytum.style.display = "none";
-  viajemic.style.display = "none";
-  esc.style.display = "none";
+function mostrarSalaTorreDM() {
+  stdm.style.display = "block";
+  ste.style.display = "none";
+  biblio.style.display = "none";
 }
 
-function mostrarJuanga() {
-  animeost.style.display = "none";
-  juanga.style.display = "block";
-  alytum.style.display = "none";
-  viajemic.style.display = "none";
-  esc.style.display = "none";
+function mostrarSalaTorreE() {
+  stdm.style.display = "none";
+  ste.style.display = "block";
+  biblio.style.display = "none";
 }
 
-function mostrarAyT() {
-  animeost.style.display = "none";
-  juanga.style.display = "none";
-  alytum.style.display = "block";
-  viajemic.style.display = "none";
-  esc.style.display = "none";
-}
-
-function mostrarViaje() {
-  animeost.style.display = "none";
-  juanga.style.display = "none";
-  alytum.style.display = "none";
-  viajemic.style.display = "block";
-  esc.style.display = "none";
-}
-
-function mostrareEscultura() {
-  animeost.style.display = "none";
-  juanga.style.display = "none";
-  alytum.style.display = "none";
-  viajemic.style.display = "none";
-  esc.style.display = "block";
+function mostrarBiblio() {
+  stdm.style.display = "none";
+  ste.style.display = "none";
+  biblio.style.display = "block";
 }
